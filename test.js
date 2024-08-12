4f8a57a77f0bcada10c46f143768ff24e52ef65a
toubadourn <domasgruzdys07@gmail.com>
2024-08-12 18:50:21 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", eventHandler);
+document.addEventListener("keydown", eventHandler);
+document.addEventListener("mousemove", eventHandler);
+document.addEventListener("scroll", eventHandler);
+document.addEventListener("resize", eventHandler);
+
+function eventHandler(event) {
+    console.log(`Event triggered: ${event.type}`);
+}
